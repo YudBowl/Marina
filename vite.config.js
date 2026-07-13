9200c90e1265b212fd840f3f783be7ab57fc5aed
YudBowl <117092979+YudBowl@users.noreply.github.com>
2026-07-14 02:53:49 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Marina/',
 })
-"scripts": {
-  "dev": "vite",
-  "build": "vite build",
-  "preview": "vite preview",
-  "predeploy": "npm run build",
-  "deploy": "gh-pages -d dist"
-}
+
